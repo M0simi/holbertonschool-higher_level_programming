0572f53cf8c851af8e7ab2934c3fa05752b88b10
Your Name <you@example.com>
2025-05-18 23:38:19 +0000

    safe print list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")  # prinnt with out new line
+            count += 1
+        except IndexError:
+            break
+    print()  # new line after print
+    return count
+    
