8a3f764f72a3eb697a66d66afef926d53b984287
Your Name <you@example.com>
2025-05-18 02:03:00 +0000

    edit

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str): 
+    if not isinstance(roman_string, str):
         return 0
 
-    roman_valuse = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                     'C': 100, 'D': 500, 'M': 1000}
     result = 0
     for i in range(len(roman_string)):
-        value = roman_valuse[roman_string[i]]
+        value = roman_values[roman_string[i]]
 
-        if i + 1 < len(roman_string) and value < roman_valuse[roman_string[i + 1]]:
+        if i + 1 < len(roman_string) and value < roman_values[
+          roman_string[i + 1]]:
             result -= value
         else:
             result += value
