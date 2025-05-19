f739590e72ec5659ed7ab5bf4d024c7b916b4b5c
Your Name <you@example.com>
2025-05-19 00:43:04 +0000

    allowed for one exception

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except(ValueError, TypeError):
             continue
-        except IndexError:
-            break  # stop the loop
+
     print()
     return count
