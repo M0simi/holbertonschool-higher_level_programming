1f98c0abe6205b7c4c6e71da011d66c101ce6564
Your Name <you@example.com>
2025-05-22 09:40:42 +0000

    task no.3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""a class that defines a square by based on 1-square.py"""
+"""THis class is with private and public instance"""
 
 
 class Square:
-    """class that have instantiation with optional"""
+    """class eith private and public inistance"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
