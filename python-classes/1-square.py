422f69633718536e245bb54b6b7dd6ab34f2ef34
Your Name <you@example.com>
2025-05-22 04:35:29 +0000

    class that defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a class that defines a square"""
+
+
+class Square:
+    """class that defines square"""
+
+    def __init__(self, size):
+
+        self.__size = size
