f673a71deb3d607935f55b96e8538888118759a1
root <you@example.com>
2025-05-25 07:05:09 +0000

    edit

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -50,5 +50,5 @@ class Rectangle:
     def perimeter(self):
         """public instance method that return the permiter"""
         if self.__width or self.__height == 0:
-            return = 0
+            return 0
         return 2 * (self.__width + self.__height)
