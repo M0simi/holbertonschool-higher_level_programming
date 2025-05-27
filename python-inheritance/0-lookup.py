cc331576519f862c93cc304d4ec43a72b68df7cd
Your Name <you@example.com>
2025-05-27 15:37:58 +0000

    add docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+This module provides a function to return the list of available
+attributes and methods of a given object.
+"""
 
 
 def lookup(obj):
-    """Return the list of available attributes and methods of an object."""
+    """
+    Return the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        A list of strings representing the object's attributes and methods.
+    """
     return dir(obj)
