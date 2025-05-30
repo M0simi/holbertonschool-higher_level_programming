28a9afe55a993da280338bac4d52295d5c210f07
Your Name <you@example.com>
2025-05-30 19:43:05 +0000

    edit

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -40,10 +40,13 @@ class Square(Rectangle):
     """a class that inherites from Rectangle"""
 
     def __init__(self, size):
-        self.__size = size
         self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
     def area(self):
         """method that return the area"""
         return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
