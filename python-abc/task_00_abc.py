fbd96d7df7c5a97126bef70c6bf6128403711fad
M0simi <mish5508@gmail.com>
2025-06-01 18:50:47 +0300

    Update task_00_abc.py

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,50 +1,22 @@
 #!/usr/bin/python3
-"""
-This module defines an abstract class 'Animal' and its
- subclasses 'Dog' and 'Cat'.
-The 'Animal' class enforces a contract that all subclasses must implement
-the 'sound' method, which returns a string representing the sound the
-animal makes.
-"""
-
 from abc import ABC, abstractmethod
 
 
 class Animal(ABC):
-    """
-    Abstract base class representing an animal.
 
-    All subclasses must implement the 'sound' method.
-    """
 
     @abstractmethod
     def sound(self):
-        """
-        Abstract method to be implemented by all subclasses.
-
-        Returns:
-            str: The sound made by the animal.
-        """
         pass
 
 
 class Dog(Animal):
-    """
-    Concrete subclass representing a dog.
-
-    Implements the 'sound' method to return 'Woof!'.
-    """
 
     def sound(self):
-        return "Woof!"
+        return "Bark"
 
 
 class Cat(Animal):
-    """
-    Concrete subclass representing a cat.
-
-    Implements the 'sound' method to return 'Meow!'.
-    """
 
     def sound(self):
         return "Meow!"
