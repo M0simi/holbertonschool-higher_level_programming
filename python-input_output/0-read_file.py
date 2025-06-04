6cc7a28a755ba5c42f9f2c728d59ba45f7d4601b
Your Name <you@example.com>
2025-06-04 23:16:14 +0000

    add docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a function that reads a text file
+and prints its content to stdout.
+"""
+
 
 def read_file(filename=""):
     with open(filename, encoding="UTF-8") as f:
