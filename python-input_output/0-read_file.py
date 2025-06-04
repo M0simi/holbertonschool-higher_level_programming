88ebf8ab59586057c88cc198a5a752694c4c654c
Your Name <you@example.com>
2025-06-04 23:21:02 +0000

    add docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,7 @@ and prints its content to stdout.
 
 
 def read_file(filename=""):
+    """read_file function that's read a file and print it"""
+
     with open(filename, encoding="UTF-8") as f:
         print(f.read(), end="")
