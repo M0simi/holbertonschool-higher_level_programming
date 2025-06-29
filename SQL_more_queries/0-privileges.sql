92aaef286da03e78d016726ae742b9f1621eacad
Your Name <you@example.com>
2025-06-29 16:46:54 +0000

    all privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- all privileges to users
 -- list the privileges
-SHOW GRANT for 'user_0d_1'@'host';
-SHOW GRANT for 'user_0d_2'@'host';
+SHOW GRANT for 'user_0d_1'@'localhost';
+SHOW GRANT for 'user_0d_2'@'localhost';
