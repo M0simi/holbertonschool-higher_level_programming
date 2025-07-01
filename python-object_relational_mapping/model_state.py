246d933d0271556e9188093e8f015d1e90a57a70
Your Name <you@example.com>
2025-07-01 04:26:16 +0000

    edit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,9 +8,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """State class that links to the MySQL table 'states'"""
-    __tablename__ = 'states'
+
+    __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
