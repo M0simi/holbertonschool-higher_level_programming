8b147fd022f399116ce2960dd27d5c261b394c4a
Your Name <you@example.com>
2025-07-01 00:03:47 +0000

    edit

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@ Script to list all states starting with 'N' from a MySQL database
 Usage: ./1-filter_states.py <username> <password> <database_name>
 """
 
-import MySQL
+import MySQLdb
 import sys
 
 if __name__ == "__main__":
@@ -16,6 +16,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", port=3306, user=username, passwd=password, db=db_name
     )
+
+    cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
