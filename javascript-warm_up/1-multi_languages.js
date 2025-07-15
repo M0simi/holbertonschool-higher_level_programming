dc4f0365e04f11eb6cbf64e44b1691a63d805c8c
Your Name <you@example.com>
2025-07-15 06:28:02 +0300

    edit

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-let c = 'C is fun';
-let python = 'Python is cool';
-let java = 'JavaScript is amazing';
+const c = 'C is fun';
+const python = 'Python is cool';
+const java = 'JavaScript is amazing';
 
-console.log(c, python, java);
+console.log(c + '\n' + python + '\n' + java);
+
+// console.log("C is fun\nPython is cool\nJavaScript is amazing");
