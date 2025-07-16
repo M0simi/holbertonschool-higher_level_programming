1a742c7a7a16de2815d930fb350469eabd55df68
M0simi <mish5508@gmail.com>
2025-07-16 03:43:43 +0300

    Update 13-model_state_delete_a.py

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,35 +5,21 @@ containing the letter 'a' from the database.
 """
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 
-if __name__ == "__main__":
-    # Check for correct arguments count
-    if len(sys.argv) != 4:
-        print("Usage: ./13-model_state_delete_a.py "
-              "<username> <password> <database>")
-        sys.exit(1)
-
-    # Connect to the database
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query for states containing 'a' in their name
-    states_to_delete = session.query(State).filter(
-        State.name.like('%a%')).all()
+    states = session.query(State).filter(State.name.like('%a%')).all()
 
-    # Delete all the states found
-    for state in states_to_delete:
+    for state in states:
         session.delete(state)
 
     session.commit()
-    session.close()
