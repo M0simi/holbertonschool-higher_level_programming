fda912c62c297c9fad14ca2239a7ee74216bd7fb
M0simi <mish5508@gmail.com>
2025-07-16 03:41:58 +0300

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,42 +1,21 @@
 #!/usr/bin/python3
 """
-List all states from the database hbtn_0e_0_usa
-that start with an uppercase 'N'.
+Lists all states with a name starting with N
 """
-
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    # Read credentials from command line arguments
-    user, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    # Connect to the database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=user,
-        passwd=password,
-        db=db_name,
-        charset="utf8",
-    )
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
 
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # Select states starting with 'N' (case-sensitive)
-    cursor.execute(
-        """
-        SELECT * FROM states
-        WHERE name LIKE 'N%'
-        ORDER BY id ASC
-    """
-    )
-
-    # Fetch and print all matching rows
-    for row in cursor.fetchall():
-        print(row)
-
-    # Clean up
-    cursor.close()
-    db.close()
+    for state in states:
+        print(state)
