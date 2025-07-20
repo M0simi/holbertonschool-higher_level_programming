5994bd184818b8e4a34d8bdf3c2a875bfd6a07c5
Your Name <you@example.com>
2025-07-20 19:21:09 +0300

    try to run it

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request
+import json
+import csv
+import sqlite3
+
+app = Flask(__name__)
+
+def read_json_file():
+    try:
+        with open('products.json', 'r') as file:
+            return json.load(file)
+    except Exception:
+        return []
+
+def read_csv_file():
+    try:
+        with open('products.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            return [row for row in reader]
+    except Exception:
+        return []
+
+def read_sqlite():
+    try:
+        conn = sqlite3.connect('products.db')
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute('SELECT id, name, category, price FROM Products')
+        rows = cursor.fetchall()
+        # change every line to dict
+        products = [dict(row) for row in rows]
+        conn.close()
+        return products
+    except Exception:
+        return []
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+    products = []
+    error = None
+
+    if source == 'json':
+        products = read_json_file()
+    elif source == 'csv':
+        products = read_csv_file()
+    elif source == 'sql':
+        products = read_sqlite()
+    else:
+        error = "Wrong source"
+        return render_template('product_display.html', error=error)
+
+    if product_id:
+        try:
+            product_id = int(product_id)
+            products = [p for p in products if int(p['id']) == product_id]
+            if not products:
+                error = "Product not found"
+        except ValueError:
+            error = "Invalid ID"
+
+    return render_template('product_display.html', products=products, error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True)
